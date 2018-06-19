335fec7ad9cdf1ad6f09620e16f208720dfe196d
spio <spio@tiguriner>
2018-06-19 13:30:47 +0200

    - improved cumulation script

--- changerates-allscripts.sh
+++ changerates-allscripts.sh
@@ -1,6 +1,6 @@
 python3 changerates.py
 
 python3 changerates-metrics.py > changerates-metrics.log
-python3 changerates-total.py
+python3 changerates-total.py 177
 python3 changerates-plot.py
 python3 changerates-total-plot.py
--- changerates-total.py
+++ changerates-total.py
@@ -1,13 +1,21 @@
 import pandas as pd
+import sys
 
 startcount = 177
 
-df = pd.read_csv("changerates.csv")
-df = df.set_index(["#date"])
+def producetotal(startcount):
+	df = pd.read_csv("changerates.csv")
+	df = df.set_index(["#date"])
 
-df["total"] = df["new"].rolling(min_periods=1, window=1).sum() - df["removed"].rolling(min_periods=1, window=1).sum()
-df["total"] = df["total"].astype(int) + 177
+	df["total"] = df["new"].rolling(min_periods=1, window=1000).sum() - df["removed"].rolling(min_periods=1, window=1000).sum()
+	df["total"] = df["total"].astype(int) + startcount
 
-print(df["total"])
+	print(df["total"])
 
-df["total"].to_csv("changerates-total.csv")
+	df["total"].to_csv("changerates-total.csv")
+
+if __name__ == "__main__":
+	if len(sys.argv) != 2:
+		print("Syntax: {} <startcount>".format(sys.argv[0]), file=sys.stderr)
+		sys.exit(1)
+	producetotal(sys.argv[1])
