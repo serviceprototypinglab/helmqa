2a76cb67de958ac95d45c2fcc9b65526bf276686
spio <spio@tiguriner>
2018-06-20 11:48:34 +0200

    - resilience: always run experiment scripts on startup

--- helmqaweb.sh
+++ helmqaweb.sh
@@ -5,4 +5,12 @@ then
 	mkdir -p logs
 fi
 
+if [ ! -d _charts ]
+then
+	python3 buckets.py
+	python3 dupestats.py _charts
+	python3 authorsets.py _charts stats
+	sh rewriter-all.sh
+fi
+
 LC_ALL=C.UTF-8 LANG=C.UTF-8 FLASK_APP=helmqaweb.py flask run --host=0.0.0.0 2>&1 | tee -a logs/helmqaweb.log
