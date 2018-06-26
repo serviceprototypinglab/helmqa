ce9f1687790656095fc19db1949f6b60efff0a9a
spio <spio@tiguriner>
2018-06-26 11:54:21 +0200

    - built-in support for coloured plots

--- changerates-plot.py
+++ changerates-plot.py
@@ -6,11 +6,11 @@ import sys
 
 FONT = 16
 
-def plot_rates():
+def plot_rates(cmap):
 	df = pd.read_csv("changerates.csv")
 	df = df.set_index(["#date"])
 
-	ax = df.plot(cmap="gray", fontsize=FONT)
+	ax = df.plot(cmap=cmap, fontsize=FONT)
 	ax.set_xlabel("date", fontsize=FONT)
 	ax.set_ylabel("chart changes", fontsize=FONT)
 	ax.set_title("Change rates of KubeApps Hub", fontsize=FONT)
@@ -23,7 +23,7 @@ def plot_rates():
 	#pylab.show()
 	pylab.savefig("changerates-plot.png")
 
-def plot_total():
+def plot_total(cmap):
 	df = pd.read_csv("changerates-total.csv", header=None, names=["#date", "total"])
 	df = df.set_index(["#date"])
 
@@ -34,14 +34,14 @@ def plot_total():
 		dfuniq = pd.read_csv("changerates-unique.csv")
 		dfuniq = dfuniq.set_index(["#date"])
 
-	ax = df.plot(cmap="gray", ylim=[0, max(df["total"]) + 50], fontsize=FONT)
+	ax = df.plot(cmap=cmap, ylim=[0, max(df["total"]) + 50], fontsize=FONT)
 	ax.set_xlabel("date", fontsize=FONT)
 	ax.set_ylabel("total charts", fontsize=FONT)
 	ax.set_title("Evolution of KubeApps Hub", fontsize=FONT)
 	ax.legend(fontsize=FONT)
 
 	if dfuniq is not None:
-		dfuniq.plot(cmap="gray", ax=ax, fontsize=FONT)
+		dfuniq.plot(cmap=cmap + "_r", ax=ax, fontsize=FONT)
 
 	pylab.setp(ax.xaxis.get_majorticklabels(), rotation=70)
 	#ax.set_xticks(range(0, len(df["counter"]), 2))
@@ -51,14 +51,19 @@ def plot_total():
 	pylab.savefig("changerates-total-plot.png")
 
 if __name__ == "__main__":
-	if len(sys.argv) != 2:
-		print("Syntax: {} <command>".format(sys.argv[0]), file=sys.stderr)
-		print("Commands: rates, total", file=sys.stderr)
+	if len(sys.argv) not in (2, 3):
+		print("Syntax: {} <command> [<colourmap>]".format(sys.argv[0]), file=sys.stderr)
+		print("Commands: rates, total / Colourmaps: e.g. gray, seismic", file=sys.stderr)
 		sys.exit(1)
+
 	command = sys.argv[1]
+	cmap = "gray"
+	if len(sys.argv) == 3:
+		cmap = sys.argv[2]
+
 	if command == "rates":
-		plot_rates()
+		plot_rates(cmap)
 	elif command == "total":
-		plot_total()
+		plot_total(cmap)
 	else:
 		print("Unknown command.", file=sys.stderr)
