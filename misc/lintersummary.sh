8b56b9c2cc86800cb3107b9db15403d94c7e13a8
spio <spio@tiguriner>
2018-09-17 23:16:35 +0200

    - support for reporting of errors in addition to warnings and infos

--- misc/lintersummary.sh
+++ misc/lintersummary.sh
@@ -1,32 +1,41 @@
 allinfo=0
 allwarning=0
+allerror=0
 chartsinfo=0
 numcharts=0
 alltxt=
 
-for i in _charts/*.tgz
-do
-	echo -n $counter
+predirs=`find -name Chart.yaml`
+dirs=
+for predir in $predirs; do x=`dirname $predir`; dirs="$dirs $x"; done
 
-	lintput=`helm lint $i`
+#dirs=_charts/*.tgz
+
+for i in $dirs
+do
+	echo -n .
+	lintput=`helm lint $i 2>&1`
 
 	infotxt=`echo $lintput | grep INFO`
 	info=`echo $lintput | grep INFO | wc -l`
 	warning=`echo $lintput | grep WARNING | wc -l`
+	error=`echo $lintput | grep Error | wc -l`
 
 	allinfo=$(($allinfo+$info))
 	allwarning=$(($allwarning+$warning))
+	allerror=$(($allerror+$error))
 	if [ $info -gt 0 ]
 	then
-		((chartsinfo++))
+		chartsinfo=$(($chartsinfo+1))
 		infotxt="$infotxt\n"
 	fi
 	alltxt="$alltxt$infotxt"
-	((numcharts++))
+	numcharts=$(($numcharts+1))
 done
 echo
 
 echo "Infos: $allinfo (Charts: $chartsinfo of $numcharts)"
 echo "Warnings: $allwarning"
+echo "Errors: $allerror"
 echo "All infos:"
 echo $alltxt
