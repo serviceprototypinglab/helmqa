429a9556d3dc711e92bd1bcbf73bda2759bf3c22
mohammed-ali-1 <mohammad10596@gmail.com>
2018-11-26 09:39:20 +0100

    configured helmqa to run in client mode

--- helmqa.sh
+++ helmqa.sh
@@ -1,10 +1,12 @@
-python3 main.py
+if [[ "$1" == "-r" ]]; then
+	python3 main.py
+fi
 
 mkdir logs
 
 unameOut="$(uname -s)"
 
-case "${unameOut}" in
+case "$unameOut" in
     Linux*) LC_ALL=C.UTF-8 LANG=C.UTF-8 FLASK_APP=helmqaweb.py flask run --host=0.0.0.0 2>&1 | tee -a logs/helmqaweb.log;;
     Darwin*) LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8 FLASK_APP=helmqaweb.py flask run --host=0.0.0.0 2>&1 | tee -a logs/helmqaweb.log;;
 esac
